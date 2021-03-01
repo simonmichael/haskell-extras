20a9d9d41a9551aadadb61cbe23c609fe754c164
Simon Michael <simon@joyful.com>
2021-03-01 07:08:17 -0800

    annotated haskell script templates

---
+++ script1.hs
@@ -0,0 +1,22 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-17.4 --verbosity info
+-- --package aeson --package csv --package tabular --package text
+-- # --compile
+
+-- Usage: script1.hs (Windows: stack script1.hs)
+-- A haskell script using stack's script command. stack must be in $PATH.
+-- The stack script command may be split across multiple lines.
+-- Sometimes packages get inferred from imports, but it's unclear; best to specify all with --package.
+-- GHC and packages from the specified stackage resolver will be auto-installed.
+-- --verbosity info shows install progress.
+-- Uncommenting --compile will auto-compile the script as needed.
+-- A .hs file extension is required.
+-- Tested with stack 2.5.1.
+
+import Data.Aeson
+import qualified Data.Text as T
+import Text.CSV
+import Text.Tabular
+
+main = do
+  putStrLn "hello"
